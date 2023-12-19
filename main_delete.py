b06a7a4ffe110642fe0ee8ab760643df66c936d8
essaiydy <imaneessaiydy2@gmail.com>
2023-12-19 15:41:28 -0800

    task 5

--- main_delete.py
+++ main_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Test delete feature
 """
+from models.place import Place
 from models.engine.file_storage import FileStorage
 from models.state import State
 
@@ -31,6 +32,12 @@ another_state.name = "Nevada"
 fs.new(another_state)
 fs.save()
 print("Another State: {}".format(another_state))
+
+new_place = Place()
+new_place.name = 'ayoub'
+fs.new(new_place)
+fs.save()
+print("Another State: {}".format(new_place))
 
 # All States
 all_states = fs.all(State)
