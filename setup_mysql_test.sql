db6b94bc3525a3e02fdf746a6a932dc27fbedf89
essaiydy <imaneessaiydy2@gmail.com>
2023-12-19 11:59:02 -0800

    task 3

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- create a database and user and grant them previliges
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+Create USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.*
+TO 'hbnb_dev'@'localhost';
