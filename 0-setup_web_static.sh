79bf835cd075293e3585fe800b4867cb868b0424
essaiydy <imaneessaiydy2@gmail.com>
2024-01-12 11:27:32 -0800

    task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,10 +4,10 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo service nginx start
-mkdir /data/web_static/releases/test/ -p
-mkdir /data/web_static/shared/ -p
+sudo mkdir /data/web_static/releases/test/ -p
+sudo mkdir /data/web_static/shared/ -p
 echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -Rh ubuntu /data/
+sudo chown -Rh ubuntu:ubuntu /data/
 sudo sed -i -E 's#server_name _;#server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}#' /etc/nginx/sites-enabled/default
-service nginx restart
+sudo service nginx restart
