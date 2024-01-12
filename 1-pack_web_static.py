6b0e9df7d79e8234bb31e14de6f4a2daa4c6546f
essaiydy <imaneessaiydy2@gmail.com>
2024-01-12 08:44:01 -0800

    t

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 '''Fabric script that generates a .tgz archive from the contents of the web_static'''
-
-
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
         '''unctin abric that generates a .tgz archive'''
-
         time = datetime.now().strftime("%Y%m%d%H%M%S")
         tarre = "tar cvzf versions/web_static_{}.tgz web_static".format(time)
 
