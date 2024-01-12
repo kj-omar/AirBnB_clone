ac4ff5354488f44e64f83c840d80c92d6e53c099
essaiydy <imaneessaiydy2@gmail.com>
2024-01-12 08:42:27 -0800

    t

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,11 +2,19 @@
 '''Fabric script that generates a .tgz archive from the contents of the web_static'''
 
 
-from fabric.api import run, local
+from fabric.api import local
 from datetime import datetime
 
 def do_pack():
-    '''unctin abric that generates a .tgz archive'''
-    run(mkdir -p /version)
-    tarf = "tar cvzf version/web_static_{}.tgz /data/web_static/".format(datetime().now().strftime("%Y%m%d%H%M%S"))
-    local(/version/tarf)
+        '''unctin abric that generates a .tgz archive'''
+
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        tarre = "tar cvzf versions/web_static_{}.tgz web_static".format(time)
+
+        local("mkdir -p versions")
+
+        res = local(tarre)
+
+        if res.failed:
+            return None
+        return f"versions/web_static_{time}.tgz"
