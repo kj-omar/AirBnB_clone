886c2e3853eb7a2d0b197bc0f1a1645f03074556
essaiydy <imaneessaiydy2@gmail.com>
2024-01-13 17:29:40 -0800

    that's good

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 '''Fabric script that generates a .tgz archive'''
 from fabric.api import local, put, run, env
 from datetime import datetime
-import os
+from os.path import exists
 
 
 def do_pack():
@@ -19,21 +19,19 @@ def do_pack():
 
 def do_deploy(archive_path):
     '''distributes an archive to your web servers'''
-    if not os.path.exists(archive_path):
-        return False
-    try:
+    if  exists(archive_path):
         ext = os.path.splitext(archive_path)[-1]
         noext = ext.split('.')[0]
         path = "/data/web_static/releases/"
 
         put(archive_path, '/tmp/')
-        run(f"mkdir -p {path}{noext}")
-        run(f"tar -xzf /tmp/{noext}.tgz -C {path}")
-        run(f"mv {path}{noext}/web_static/* {path}{noext}/")
-        run(f"rm -rf {path}{noext}/web_static/")
+        run("mkdir -p {}{}".format(path, noext))
+        run("tar -xzf /tmp/{}.tgz -C {}".format(noext, path))
+        run("rm /tmp/{}".format(ext))
+        run("mv {}{}/web_static/* {}{}/".format(path, noext, path,noext))
+        run("rm -rf {}{}/web_static/".format(path, noext))
         run("rm -rf /data/web_static/current")
-        run(f"ln -s {path}{noext} /data/web_static/current")
+        run("ln -s {}{} /data/web_static/current".format(path, noext))
         print("New version deployed!")
         return True
-    except:
-        return False
+    return False
