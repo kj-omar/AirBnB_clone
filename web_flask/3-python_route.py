4c2bf6a4f451e0f634d7887b074bdb98038dd3e3
essaiydy <imaneessaiydy2@gmail.com>
2024-01-27 04:12:33 -0800

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,11 +25,10 @@ def c_is_fun(text):
 
 
 @app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', defaults={'text' : 'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 def is_cool(text):
     '''display Python followed by the value of the text variable'''
     return "Python {}".format(text.replace("_", " "))
-
 
 
 if __name__ == '__main__':
