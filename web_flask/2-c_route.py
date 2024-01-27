2e297bbc07d6f6dd321c2db7225bc392466d0a6b
essaiydy <imaneessaiydy2@gmail.com>
2024-01-27 03:57:32 -0800

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def HBNB():
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     '''function display a text'''
-    return f'C {text}'
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
