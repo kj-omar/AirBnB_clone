c643d801d3c0e651bcb9c792d6fc9b7013e13d98
essaiydy <imaneessaiydy2@gmail.com>
2024-01-27 03:28:52 -0800

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_hbnb():
     '''function display display “Hello HBNB!”'''
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     '''function display display “HBNB”'''
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
