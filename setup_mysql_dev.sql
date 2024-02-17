c96422ca9d3c88bb0d7c7e566ec39d4284a8fd86
hafssaelk <138696982+hafssaelk@users.noreply.github.com>
2024-02-17 16:46:36 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,18 +1,12 @@
--- Drop the user if it exists
-DROP USER IF EXISTS 'hbnb_dev'@'localhost';
-
--- Create the user
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant SHOW DATABASES privilege to the user
-GRANT SHOW DATABASES ON *.* TO 'hbnb_dev'@'localhost';
-
--- Create the database if it doesn't exist
+-- this script prepares a MySQL server for the project
+-- create project developement database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Grant privileges to the user
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it dosen't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-SHOW GRANTS FOR 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hafssaelk';
-SELECT user FROM mysql.user WHERE user = 'hbnb_dev' AND host = 'localhost';
+FLUSH PRIVILEGES;
