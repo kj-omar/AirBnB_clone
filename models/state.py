f3c94afc2dd7e70d7e6514658294aa64440f20a4
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-03-27 23:03:07 +0400

    BaseModel, City and State classes was updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from models.city import City
+from models import storage
 
 
-class State(BaseModel):
+Base = declarative_base()
+
+class State(BaseModel, Base):
     """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref=backref("state", cascade="all, delete"))
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of City instances
+        with state_id equals to the current State.id"""
+        city_instances = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_instances.append(city)
+        return city_instances
     name = ""
