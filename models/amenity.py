a2609f86aaff47cf6beddd2c92d859eeb1098acc
Spacefox95 <nathan.raynal@gmail.com>
2024-03-28 17:24:43 +0100

    Errasing some mistake

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel):
+    """ Amenity class"""
+    __tablename__ = "amenities"
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity", backref="")
+
 
     name = ""
--- models/place.py
+++ models/place.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """ A place to stay """
+    __tablename__ = "place_amenity"
     city_id = ""
     user_id = ""
     name = ""
