88fa84e68ac4d5234dc33259092c5310a9f16f71
Sebas-crypt <usefullseba17@gmail.com>
2024-03-29 15:47:51 -0400

    fixng

--- models/user.py
+++ models/user.py
@@ -2,7 +2,6 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -12,8 +11,3 @@ class User(BaseModel, Base):
     password = Column('password', String(128), nullable=False)
     first_name = Column('first_name', String(128), nullable=True, default="NULL")
     last_name = Column('last_name', String(128), nullable=True, default="NULL")
-    # backref may need to be back_populates?Below line commented out bc console
-    # would not run with it in. This line was implemented in Task 8
-    places = relationship("Place", cascade="delete", backref="user")
-    # Below line is commented out for caution and was added in Task 9
-    reviews = relationship("Review", cascade="delete", backref="user")
