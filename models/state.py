ac80315858862873a210c49a0df38c68fc6624dc
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-03-29 18:24:21 +0400

    fix needed but commited #2

--- models/state.py
+++ models/state.py
@@ -14,13 +14,13 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref=backref("state", cascade="all, delete"))
-    @property
-    def cities(self):
-        from models import storage
-        """Getter attribute cities that returns the list of City instances
-        with state_id equals to the current State.id"""
-        city_instances = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_instances.append(city)
-        return city_instances
+    # @property
+    # def cities(self):
+    #     from models import storage
+    #     """Getter attribute cities that returns the list of City instances
+    #     with state_id equals to the current State.id"""
+    #     city_instances = []
+    #     for city in storage.all(City).values():
+    #         if city.state_id == self.id:
+    #             city_instances.append(city)
+    #     return city_instances
