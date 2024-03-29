a794aca10f1e51dd3f4743c43dc36962f58008c7
khalilBahi <7753@holbertonstudents.com>
2024-03-29 10:14:13 +0100

    mm

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- A comment
+-- A comment eee
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
