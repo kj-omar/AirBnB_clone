b0fd13fdfb425f12def4feaa0c8bf8c52ecab10a
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-29 23:12:02 +0100

    Updated testDBStorage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Module for testing file storage"""
+from io import StringIO
 import os
 import MySQLdb
 import unittest
+from models import storage
 from models.city import City
 from models.state import State
 from unittest.mock import patch, MagicMock
@@ -12,73 +14,70 @@ from models.engine.db_storage import DBStorage
 class TestDBStorage(unittest.TestCase):
     """ Test class for dbstorage """
 
+    # def __init__(self, *args, **kwargs):
+    #     """ Test filestorage instantiation """
+    #     storage = DBStorage()
+    #     storage.reload()
+
     def setUp(self):
         """ Test set up """
-        super().setUp()
-        if os.getenv('HBNB_ENV') == 'test' and os.getenv(
-                'HBNB_TYPE_STORAGE') == 'db':
-            self.db = MySQLdb.connect(host="localhost",
-                                      user="hbnb_test",
-                                      passwd="hbnb_test_pwd",
-                                      database="hbnb_test_db")
-            self.cursor = self.db.cursor()
-
+        pass
+    
     def tearDown(self):
         """Test removing json file or closing database connection"""
-        super().tearDown()
-        if os.getenv('HBNB_ENV') == 'test' and os.getenv(
-                'HBNB_TYPE_STORAGE') == 'db':
-            self.cursor.close()
-            self.db.close()
+        pass
 
     @patch('models.engine.db_storage.DBStorage.all')
     def test_all_method(self, mock_all):
-        mock_all.return_value = {
-            'State.1': State(id='1', name='California'),
-            'City.1': City(id='1', name='Los Angeles', state_id='1')
-        }
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            mock_all.return_value = {
+                'State.1': State(id='1', name='California'),
+                'City.1': City(id='1', name='Los Angeles', state_id='1')
+            }
 
-        storage = DBStorage()
-        all_objects = storage.all()
+            all_objects = storage.all()
 
-        self.assertIn('State.1', all_objects)
-        self.assertIn('City.1', all_objects)
-        self.assertIsInstance(all_objects['State.1'], State)
-        self.assertIsInstance(all_objects['City.1'], City)
+            self.assertIn('State.1', all_objects)
+            self.assertIn('City.1', all_objects)
+            self.assertIsInstance(all_objects['State.1'], State)
+            self.assertIsInstance(all_objects['City.1'], City)
 
     @patch('models.engine.db_storage.DBStorage._DBStorage__session',
            new_callable=MagicMock)
     def test_new_method(self, mock_session):
         """Test the new() method"""
-        new_state = State(name="New York")
-        storage = DBStorage()
-        storage.new(new_state)
-        mock_session.add.assert_called_with(new_state)
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            new_state = State(name="New York")
+            storage.new(new_state)
+            mock_session.add.assert_called_with(new_state)
 
     @patch('models.engine.db_storage.DBStorage._DBStorage__session',
            new_callable=MagicMock)
     def test_save_method(self, mock_session):
         """Test the save() method"""
-        new_state = State(name="Florida")
-        storage = DBStorage()
-        storage.new(new_state)
-        storage.save()
-        mock_session.commit.assert_called_once()
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            new_state = State(name="Florida")
+            storage.new(new_state)
+            storage.save()
+            mock_session.commit.assert_called_once()
 
     @patch('models.engine.db_storage.DBStorage._DBStorage__session',
            new_callable=MagicMock)
     def test_delete_method(self, mock_session):
         """Test the delete() method"""
-        state_to_delete = State(name="Texas")
-        storage = DBStorage()
-        storage.new(state_to_delete)
-        storage.delete(state_to_delete)
-        mock_session.delete.assert_called_with(state_to_delete)
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            state_to_delete = State(name="Texas")
+            storage.new(state_to_delete)
+            storage.delete(state_to_delete)
+            mock_session.delete.assert_called_with(state_to_delete)
 
-    @patch('builtins.print')
     @patch('models.storage', new_callable=MagicMock)
-    def test_create_state_db_storage(self, mock_storage, mock_print):
+    def test_create_state_db_storage(self, mock_storage):
         """Test creating a state with the console using DBStorage"""
-        self.console.onecmd("create State name=\"Louisiana\"")
-        mock_storage.new.assert_called()
-        mock_storage.save.assert_called_once()
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            with patch('sys.stdout', new=StringIO()) as f:
+                self.console.onecmd("create State name=\"Louisiana\"")
+                self.assertNotEqual(f.getvalue(), "** class doesn't exist **\n")
+                self.assertNotEqual(f.getvalue(), "** class name missing **\n")
+                mock_storage.new.assert_called()
+                mock_storage.save.assert_called_once()
