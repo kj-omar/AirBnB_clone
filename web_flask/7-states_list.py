fb8e5f07c0f62b91005da808a889bf9dcd41bbb9
ClintonCMOs <orengecmo@gmail.com>
2024-03-29 13:57:10 +0300

    updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 """ A script that starts a Flask web application """
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """
+    Renders states_list htmf to display states created.
+    """
+    path = '7-states_list.html'
+    states = storage.all(State)
+    s = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, s=s)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exception_object):
+    """
+    Used for cleaning up resources or performing actions after
+    a request has been processed.
+    """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port=5000)
