8a8241caefab5ef6ce86e39e8d1bff93029df2cb
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-03-30 18:17:05 +0400

    DBStorage - Place added

--- models/city.py
+++ models/city.py
@@ -4,11 +4,12 @@ from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
-
-
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="cities")
