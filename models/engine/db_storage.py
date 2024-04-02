2febb4097b4eb8d8717aac0454746db3db2863cc
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-03 02:40:09 +0400

    pycodestyle engine fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.base_model import Base
@@ -12,6 +14,7 @@ from models.user import User
 from sqlalchemy.orm import scoped_session
 import os
 
+
 class DBStorage:
     """This class manages storage of hbnb models"""
     __engine = None
@@ -21,11 +24,11 @@ class DBStorage:
         """Creates a new FileStorage instance"""
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                      .format(os.getenv('HBNB_MYSQL_USER'),
-                          os.getenv('HBNB_MYSQL_PWD'),
-                          os.getenv('HBNB_MYSQL_HOST'),
-                          os.getenv('HBNB_MYSQL_DB')),
-                      pool_pre_ping=True)
+                                      .format(os.getenv('HBNB_MYSQL_USER'),
+                                              os.getenv('HBNB_MYSQL_PWD'),
+                                              os.getenv('HBNB_MYSQL_HOST'),
+                                              os.getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
@@ -40,12 +43,12 @@ class DBStorage:
             objects = []
             for cls in classes:
                 objects += self.__session.query(cls).all()
-        
+
         result = {}
         for obj in objects:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             result[key] = obj
-        
+
         return result
 
     def new(self, obj):
@@ -54,8 +57,8 @@ class DBStorage:
 
     def save(self):
         """Saves storage to file"""
-        self.__session.commit() 
-    
+        self.__session.commit()
+
     def delete(self, obj=None):
         """Deletes obj from storage"""
         if obj:
@@ -65,4 +68,4 @@ class DBStorage:
         """Loads storage from file"""
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(Session)() 
+        self.__session = scoped_session(Session)()
