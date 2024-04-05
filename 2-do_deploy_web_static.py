6f2c1f2e34f9af9bea3541dffad375f21b68b2d2
DennisMakaka <dennismakaka078@gmail.com>
2024-04-04 19:16:53 -0700

    Full deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,79 +1,51 @@
 #!/usr/bin/python3
-"""
-This Python module facilitates the deployment of archives to web servers.
-"""
-
-from os import path
-
+"""Distributes an archive to web servers using the do_deploy function."""
+from fabric.contrib import files
 from fabric.api import env, put, run
+import os
 
-# Define the server environment
-env.hosts = ["18.206.207.38", "3.86.7.50"]
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/school"
+# Define the host servers
+env.hosts = ['18.209.152.50', '34.229.136.68']
+
 
 def do_deploy(archive_path):
-    """
-    Deploy the web files to the server.
-
-    Args:
-        archive_path (str): Path to the archive file to be deployed.
-
-    Returns:
-        bool: True if deployment is successful, False otherwise.
-    """
-    try:
-        # Check if the archive file exists
-        if not (path.exists(archive_path)):
-            return False
-
-        # Transfer the archive file to the remote server
-        put(archive_path, "/tmp/")
-
-        # Extract the archive file to a specific directory
-        timestamp = archive_path[-18:-4]
-        run(
-            "sudo mkdir -p /data/web_static/\
-releases/web_static_{}/".format(
-                timestamp
-            )
-        )
-        run(
-            "sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/".format(
-                timestamp, timestamp
-            )
-        )
-
-        # Remove the temporary archive file
-        run("sudo rm /tmp/web_static_{}.tgz".format(timestamp))
-
-        # Move the extracted content to its destination
-        run(
-            "sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/".format(
-                timestamp, timestamp
-            )
-        )
-
-        # Remove the previous symbolic link
-        run(
-            "sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static".format(
-                timestamp
-            )
-        )
-
-        # Update the symbolic link to point to the new deployment
-        run("sudo rm -rf /data/web_static/current")
-        run(
-            "sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current".format(
-                timestamp
-            )
-        )
-    except Exception as e:
+    """Deploy the archive to the web servers."""
+    # Check if the archive exists
+    if not os.path.exists(archive_path):
         return False
 
-    return True
+    # Define the destination path on the server
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
+    try:
+        # Upload the archive to the /tmp directory on the server
+        put(archive_path, '/tmp')
+
+        # Create the necessary directories on the server
+        run('mkdir -p {}'.format(dest))
+
+        # Extract the archive to the destination directory
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+
+        # Remove the uploaded archive from the server
+        run('rm -f /tmp/{}.tgz'.format(name))
+
+        # Move the contents of the extracted archive to the destination directory
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+
+        # Remove the web_static directory from the destination directory
+        run('rm -rf {}/web_static'.format(dest))
+
+        # Remove the current symlink if it exists
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symlink to the deployed version
+        run('ln -s {} /data/web_static/current'.format(dest))
+
+        return True
+    except Exception:
+        return False
 
