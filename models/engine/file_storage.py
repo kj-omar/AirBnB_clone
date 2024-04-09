df26f55c3949c902994cce29014b81e3ab2c0f38
camilof91 <camilofetecua2011@outlook.com>
2024-04-09 16:13:21 -0500

    added delete methot on file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,9 +14,7 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -50,3 +48,13 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside
+        """
+        if obj:
+            key = f"{type(obj).__name__}.{obj.id}"
+
+            if self.__objects[key]:
+                del self.__objects[key]
+                self.save()
