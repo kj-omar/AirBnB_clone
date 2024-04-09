f84806abcc3d49520bd4b08a4e37473d01b2b117
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-09 17:05:30 +0200

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='./templates')
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def remove_current_session(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
