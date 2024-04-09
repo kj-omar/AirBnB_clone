b380837eac018b87ad5d10b7c25b8058cee62b22
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-09 10:22:30 +0200

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='./templates')
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace("_", " ")
+    return f'C {text}'
+
+
+@app.route('/python', strict_slashes=False, defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    text = text.replace("_", " ")
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def python_number(n):
+    try:
+        return f'{n} is a number'
+    except TypeError:
+        return f'{n} must be an integer'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    try:
+        return render_template('5-number.html', n=n)
+    except TypeError:
+        return f'{n} must be an integer'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
