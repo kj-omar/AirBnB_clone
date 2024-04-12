6daf3735cc5ef7637e10963efc0177de58f8bf21
leila7575 <leila.louajri@gmail.com>
2024-04-12 22:50:24 +0200

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application."""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def HelloHBNB():
+    """display Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB!"""
+    return 'HBNB!'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """display c and replace underscore _ symbols with a space """
+    return 'C' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
