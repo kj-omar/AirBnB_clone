86bbfbc63de4be360ebbc5a2c4776e27c304b3ab
Julieed-971 <julie.dedieu1@gmail.com>
2024-04-12 15:41:15 +0200

    Refactorised code

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,12 +8,8 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='./templates')
 
 
-def load_states():
-    return sorted(storage.all(State).values(), key=lambda x: x.name)
-
-
 @app.teardown_appcontext
-def remove_current_session(exception):
+def teardown(exception):
     """Close the SQLAlchemy session"""
     storage.close()
 
@@ -21,17 +17,16 @@ def remove_current_session(exception):
 @app.route('/states', strict_slashes=False)
 def states_list():
     """Get the list of all states"""
-    states = load_states()
+    states = storage.all(State)
     return render_template('9-states.html', states=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_id_found(id):
     """Get the list of cities related to the state id"""
-    states = load_states()
+    states = storage.all(State).values()
     for state in states:
         if state.id == id:
-            state.cities = sorted(state.cities, key=lambda y: y.name)
             return render_template('9-states.html', state=state)
     return render_template('9-states.html')
 
