1842e27b6786a6408548ddd7211d1f1e5181ce56
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-14 23:20:16 +0400

    handling number temp for html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -45,7 +45,7 @@ def number_template(n):
         return render_template('5-number.html', n=n)
     else:
         abort(404)
-    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
