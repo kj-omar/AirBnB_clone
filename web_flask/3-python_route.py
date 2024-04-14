afde0397ce5a283117907c880a41c5c53d43223d
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-14 19:21:30 +0400

    0 1 2 tasks fixed the pycode style , then creating the directory for python in 3rd task

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,21 +6,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_main():
     """Display Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """Display Hello HBNB!"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def profile(text):
     text = text.replace('_', ' ')
     return f'C {text}'
 
+
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def profile_python(text='is cool'):
     text = text.replace('_', ' ')
