c9b925568b336530f18c0969ccabc4191e2ce350
Wambuuyy <wambuiprudy@gmail.com>
2024-04-15 15:57:10 +0300

    corre ted the files for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
-if ! command -v nginx &> /dev/null; then
-	sudo apt-get install -y nginx
-fi
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# the fake file
-echo "<html><head></head><body>Hoberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# use -sf to fprcefully create or overwrite if currently present
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Give ownership of the /data/ folder to the ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve /data/web_static/current/ to hbnb_static using alias
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/
-		{\n\t\talias /data/web_static/current/;\n\t}'
-			/etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-#restart nginx to apply chaanges
 sudo service nginx restart
-
-exit 0
