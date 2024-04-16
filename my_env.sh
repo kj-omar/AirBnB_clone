9202928dfc925fdfc6b173696f9b40305a7813d2
Kymemia <kimemiachristopherr@gmail.com>
2024-04-16 15:35:04 +0300

    3.mySQL setup development

---
+++ my_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export HBNB_ENV="dev"
+export HBNB_MYSQL_USER="mysql_user"
+export HBNB_MYSQL_PWD="mysql_password"
+export HBNB_MYSQL_HOST="mysql_host"
+export HBNB_MYSQL_DB="mysql_database"
+export HBNB_TYPE_STORAGE="file"
