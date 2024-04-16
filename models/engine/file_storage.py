4f2f1307b2621baf94eb251cc6145e322b50710c
Peacechimamaka <peaceamaka111@gmail.com>
2024-04-16 13:34:48 -0700

    task 4

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -24,6 +24,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        '''deletes object from __object'''
+        if obj is not None and obj in self.__objects.values():
+            for key, value in self.__objects.items():
+                if value == obj:
+                    del self.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
