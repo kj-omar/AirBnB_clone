4d8402bb6e387e491f7b314bfb4c77262f53bfe1
JohnIanOngayi <ongayijohnian@gmail.com>
2024-04-16 08:22:32 +0300

    Updating all() method for class FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        # return FileStorage.__objects
+        if cls is None:
+            return (FileStorage.__objects)
+        else:
+            return_dict = dict()
+            for k in FileStorage.__objects.keys():
+                if k.split('.')[0] == cls.__name__:
+                    return_dict[k] = FileStorage.__objects[k]
+            return (return_dict)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +56,9 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None) -> None:
+        """Deletes an object from storage"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        if key in FileStorage.__objects.keys():
+            del (FileStorage.__objects[key])
