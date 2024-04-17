57147f7ad579f8fae8645f910f491f32f7bf9a07
josfam <josephamukun@tutanota.com>
2024-04-17 04:20:46 +0000

    Fix: grant SELECT on just performance_schema db

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,4 +6,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all privileges for hbnb_test_db db
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- grant only SELECT privileges on performance_schema db
-GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
