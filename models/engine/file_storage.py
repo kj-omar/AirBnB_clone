9d98ef18682c4fe73450299d6a764482e36019ec
Peacechimamaka <peaceamaka111@gmail.com>
2024-04-17 06:55:09 -0700

    task 4

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,7 +30,7 @@ class FileStorage:
         '''deletes object from __object'''
         print("befor delete", FileStorage.__objects)
         key = f"{obj.to_dict()['__class__']}.{obj.id}"
-        if key is not None and key in self.__objects.keys():
+        if key is not None and key in self.__objects.keys:
             del FileStorage.__objects[key]
             self.save()
             print()
