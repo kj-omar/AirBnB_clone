47b292a0e2c958993c2859f9ac7e8346ce769e05
Itohan <momoduitohan5@gmail.com>
2024-04-18 00:28:04 +0100

    everything

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,7 @@
 -- Write a script that prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-DROP DATABASE IF EXISTS hbnb_dev_db;
 FLUSH PRIVILEGES;
