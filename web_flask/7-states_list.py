cc03307b79617b392e058a6b83b3f0d23d31415b
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-17 17:52:55 +0400

    States were listed alphabetically

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+"""
+This module defines a Flask application that serves a
+"Hello HBNB!" message.
+"""
 
-
-from flask import Flask, render_template, abort
+from flask import Flask
+from flask.templating import render_template
 from models import storage
-from models.state import State
 
-app = Flask(__name__)
+app = Flask("__name__", template_folder="web_flask/templates")
+
 
 @app.teardown_appcontext
-def tear_down(exception):
-    storage.close()
+def teardown(exception):
+    """
+    Closes the current SQLAlchemy session.
+    """
+    try:
+        storage.close()
+    except exception as e:
+        print(e)
 
 
-@app.route('/states_list', strict_slashes=False)
-def list_state():
-    """states"""
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """
+    Renders an HTML page that displays a list of all State objects.
+    """
     states = storage.all("State").values()
     states = sorted(states, key=lambda state: state.name)
     return render_template("7-states_list.html", states=states)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
