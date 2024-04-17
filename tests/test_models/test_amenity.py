1d613d9e22493d7de962ce6eb4b78da0a1c0d4d3
alpine57 <kamutileeon@gmail.com>
2024-04-17 02:03:13 -0700

    test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Test cases for the Amenity class"""
+
+import unittest
+from tests.test_models.test_base_model import TestBaseModel
 from models.amenity import Amenity
 
+class TestAmenity(TestBaseModel):
+    """Test cases for the Amenity class"""
 
-class test_Amenity(test_basemodel):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
+    def setUp(self):
+        """Set up test objects"""
+        super().setUp()
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name2(self):
-        """ """
+    def test_name(self):
+        """Test name attribute"""
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+if __name__ == '__main__':
+    unittest.main()
+
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Test cases for the City class"""
+
+import unittest
+from tests.test_models.test_base_model import TestBaseModel
 from models.city import City
 
+class TestCity(TestBaseModel):
+    """Test cases for the City class"""
 
-class test_City(test_basemodel):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
+    def setUp(self):
+        """Set up test objects"""
+        super().setUp()
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """Test state_id attribute"""
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ """
+        """Test name attribute"""
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+if __name__ == '__main__':
+    unittest.main()
+
