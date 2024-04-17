10db98b5d2ad1fbc73342efaf8501af7ce55a4cd
josfam <josephamukun@tutanota.com>
2024-04-17 04:18:24 +0000

    Implement `4. MySQL setup test`

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- setup test database, and corresponding user(s)
+-- create the test database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- create the user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant all privileges for hbnb_test_db db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- grant only SELECT privileges on performance_schema db
+GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
