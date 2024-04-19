8be5446c1e57d9e3539421d92027edfbacc39113
Eunice-Serani <eaamable91@gmail.com>
2024-04-19 10:38:23 +0000

    hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask("__name__")
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def hello():
     """
-    Route /
-    Displays: 'Hello HBNB'
+    Return a giving string
+
     """
-    return "Hello HBNB!"
+    return render_template("10-hbnb_filters.html")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
