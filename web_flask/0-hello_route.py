7e4338857282a990142eebb1d12aa5503e993341
Thugg55 <tuggeru7@gmail.com>
2024-04-20 09:27:20 -0700

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 # Script starts flask application, listening 0.0.0.0:5000.
 
-pip install flask
 
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def home{}: #view for home
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    app.run(strict_slashes=False)
+    app.run(host='0.0.0.0', port=5000)
