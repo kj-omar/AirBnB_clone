b5fe5b66c3b193bb1cf9cb4732fde09315a4a9ba
Thugg55 <tuggeru7@gmail.com>
2024-04-20 10:12:04 -0700

    still on work

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,16 +3,16 @@
 
 
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
+""" display Hello HBNB! on this route """
 def home{}:
     """ my web server home page """
     return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
+    """ run the app on host 0.0.0.0, port 5000 """
     app.run(host='0.0.0.0', port=5000)
