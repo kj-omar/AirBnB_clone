51ce79916706efe8b0816e20468f6ee7851c5839
Thugg55 <tuggeru7@gmail.com>
2024-04-20 10:20:07 -0700

    doing

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" Script starts flask application, listening 0.0.0.0:5000 """
+"""Script starts flask application, listening 0.0.0.0:5000
+"""
 
 
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-""" display Hello HBNB! on this route """
+"""display Hello HBNB! on this route"""
 def home{}:
-    """ my web server home page """
+    """my web server home page"""
     return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
-    """ run the app on host 0.0.0.0, port 5000 """
+    """run the app on host 0.0.0.0, port 5000"""
     app.run(host='0.0.0.0', port=5000)
