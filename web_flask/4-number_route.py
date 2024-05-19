660c1c14a0c74d07c275b2052be0e1a21b0ece5c
Mohsine Hourmat Allah <125808498+mohsinehour@users.noreply.github.com>
2024-05-19 13:03:09 +0100

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -33,7 +33,7 @@ def py_text(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """ displays n is a number """
-    return f"{n} is a number"
+    return "%d is a number" % n
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
