9197c3526654153f52d040bc548373822d8c505b
keziahNsoh <porepeya@gmail.com>
2024-06-25 01:23:16 -0700

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+# models/review.py
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
+class Review(BaseModel, Base):
+    """Review class representing a review for a place by a user."""
+    __tablename__ = 'reviews'
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Review object."""
+        super().__init__(*args, **kwargs)
