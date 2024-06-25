f583fefbb3894f8b56be45151de2b77b538bfc39
keziahNsoh <porepeya@gmail.com>
2024-06-25 02:31:55 -0700

    solution to solution 9

--- models/review.py
+++ models/review.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-# models/review.py
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""
+Review Module - contains the Review class
+"""
+
 from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 class Review(BaseModel, Base):
-    """Review class representing a review for a place by a user."""
+    """
+    Review class inherits from BaseModel and Base.
+    Represents a review for a place by a user.
+    """
+
     __tablename__ = 'reviews'
 
     text = Column(String(1024), nullable=False)
@@ -13,5 +19,7 @@ class Review(BaseModel, Base):
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Initialize Review object."""
+        """
+        Initializes Review object.
+        """
         super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
+"""
+User Module - contains the User class
+"""
+
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
-
+from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", cascade="all, delete", back_populates="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """
+    User class inherits from BaseModel and Base.
+    Represents a user of the system.
+    """
+
+    __tablename__ = 'users'
+
+    reviews = relationship('Review', cascade='all, delete-orphan', backref='user')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes User object.
+        """
+        super().__init__(*args, **kwargs)
+
