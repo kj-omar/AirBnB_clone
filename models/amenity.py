b3df9dc108c843e0d7dd91caa713e511bbabdd4f
keziahNsoh <porepeya@gmail.com>
2024-06-25 04:39:55 -0700

    Task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
-# Association table for Many-to-Many relationship between Place and Amenity
-place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
-)
 
-class Amenity(BaseModel, Base):
-    """ Amenity class """
-    __tablename__ = "amenities"
-    
-    name = Column(String(128), nullable=False)
-
-    # Relationship with Place using place_amenity table
-    place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
-
+class Amenity(BaseModel):
+    name = ""
