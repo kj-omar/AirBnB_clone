b20d484179f20e5002ea728405c61f6bf3c75641
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-06-25 15:36:19 +0100

    using db as storage

--- models/base_model.py
+++ models/base_model.py
@@ -22,19 +22,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                setattr(self, 'id', str(uuid.uuid4()))
+            if 'created_at' not in kwargs:
+                setattr(self, 'created_at', datetime.now())
+            if 'updated_at' not in kwargs:
+                setattr(self, 'updated_at', datetime.now())
 
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
