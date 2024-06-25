2452f7c25fc6efbc3aef0340065fc58f932f6434
keziahNsoh <porepeya@gmail.com>
2024-06-25 22:49:30 +0000

    Updated amenity and place models; deleted place_amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
-class Amenity(BaseModel, Base):
-    """ Amenity class """
-    __tablename__ = "amenities"
-    
-    name = Column(String(128), nullable=False)
 
-    # Define the many-to-many relationship with Place
-    place_amenities = relationship(
-        "Place",
-        secondary=place_amenity,
-        back_populates="amenities",
-        viewonly=False
-    )
-
+class Amenity(BaseModel):
+    name = ""
--- models/place.py
+++ models/place.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import Column, String, Integer, Float
+from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from models.base_model import BaseModel, Base
+from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-
     if storage_type == "db":
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable= False, default=0)
+        max_guest = Column(Integer, nullable=False, default= 0)
+        price_by_night = Column(Integer, nullable=False, default = 0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-
-        # Define the many-to-many relationship with Amenity
-        amenities = relationship(
-            "Amenity",
-            secondary=place_amenity,
-            back_populates="place_amenities",
-            viewonly=False
-        )
-
         user = relationship("User", back_populates="places")
         cities = relationship("City", back_populates="places")
     else:
@@ -45,4 +36,3 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
-
