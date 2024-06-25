7d37bd6bab4f29587d97852f5e263904a1ce586d
Joseph <josephomoruwou@gmail.com>
2024-06-25 06:19:24 -0700

    modified file: made syntax corrections(backticks not quotes)

--- setup_mysql_test_.sql
+++ setup_mysql_test_.sql
@@ -2,13 +2,13 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create new user
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY hbnb_test_pwd;
+CREATE USER IF NOT EXISTS `hbnb_test`@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant privileges
-GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT Privilege
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 
 -- Apply the changes
 FLUSH PRIVILEGES;
