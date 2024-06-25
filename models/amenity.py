54417b89cb203983e3e484d68d850903a98ed2f3
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-06-25 20:04:02 +0100

    db relationships: one-to-many

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, storage_engine
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel):
-    name = ""
+    """ The Amenity class, contains name """
+    __tablename__ = 'amenities'
+    if storage_engine == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place',
+                                       secondary=place_amenity,
+                                       back_populates='amenities')
+    else:
+        name = ""
