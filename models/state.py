8f94287003989962d142fb88a246bad15b1535b1
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-06-25 13:12:49 +0100

    update models using sqlalchemy orm mapper

--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, storage_engine
+from sqlalchemy import Column, String
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if storage_engine == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
