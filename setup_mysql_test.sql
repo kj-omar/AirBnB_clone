34319edad5f35077dc1b0c699052c137489d7c44
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-26 18:36:27 +0200

    Updated the setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create a user for the project
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
--- Grant all privileges on the project database to the project user
+-- Grant all privileges on the project database to the user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
 GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
 -- Flush privileges
