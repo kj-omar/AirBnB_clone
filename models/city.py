a6aa2ee4e173b003b82c49c47ae7bdb1743a2257
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-27 00:12:58 +0200

    Update: 8. DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -15,5 +15,7 @@ class City(BaseModel, Base):
         name (str): The name of the city.
     """
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
