f124b634e8fae80bd9b8435afc73aa841521930b
Terminal <terminalkarabo@gmail.com>
2024-06-26 03:15:51 -0700

    console debugged?

--- console.py
+++ console.py
@@ -120,18 +120,18 @@ class HBNBCommand(cmd.Cmd):
             if not args:
                 raise SyntaxError()
             arg_list = args.split(" ")
-            kw = {}
+            _dict = {}
             for arg in arg_list[1:]:
                 arg_splited = arg.split("=")
                 arg_splited[1] = eval(arg_splited[1])
                 if type(arg_splited[1]) is str:
                     arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
-                kw[arg_splited[0]] = arg_splited[1]
+                _dict[arg_splited[0]] = arg_splited[1]
         except SyntaxError:
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
+        new_instance = HBNBCommand.classes[arg_list[0]](**_dict)
         new_instance.save()
         print(new_instance.id)
 
