2a1b19558ffc26e7f767fe29c146e4df5ebfdc4f
stringpappy <jejeodunayo70@gmail.com>
2024-06-26 14:34:40 -0700

    DBStorage

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from models import storage
 
 Base = declarative_base()
 
