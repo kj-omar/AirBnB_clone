3be4295d547b4f2282ea9cb0399b4ffb23920bea
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-06-26 18:42:19 +0100

    update place

--- models/place.py
+++ models/place.py
@@ -6,15 +6,14 @@ from sqlalchemy.orm import relationship
 import os
 
 storage_engine = os.getenv("HBNB_TYPE_STORAGE")
-place_amenity = None
-if storage_engine == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True, nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True, nullable=False))
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
