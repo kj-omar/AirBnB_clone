ee35f12e1ab41cdfedd35852f3b50d4d643c07d3
Terminal <terminalkarabo@gmail.com>
2024-06-26 05:26:07 -0700

    circular import fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
-from os import getenv
+import os
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
@@ -12,11 +12,8 @@ from models.place import Place
 from models.review import Review
 
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
-
-if storage_type == 'db':
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
 else:
     storage = FileStorage()
-
 storage.reload()
