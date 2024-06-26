f4040e90c137e811e3deaebd0849ff74c96670c9
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-26 23:52:11 +0200

    Update __init__.py: (models/__init__.py)

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", 
+                          cascade="all, delete, delete-orphan"
+                          )
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of
+        City instances with state_id equals to the current 
+        State.id"""
+        from models import storage
+        from models.city import City
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
