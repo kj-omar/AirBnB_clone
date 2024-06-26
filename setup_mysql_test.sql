6f38c5781d123791131117f6cc9e6b1eaa9135c0
root <root@DESKTOP-8SR6O99>
2024-06-26 18:27:55 +0200

    Fix (File: setup_mysql_test.sql): Correction on the test_db

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create a user for the project
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant all privileges on the project database to the project user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_test@localhost;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
 GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
 -- Flush privileges
 FLUSH PRIVILEGES;
