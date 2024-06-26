ee054552e61102a81e7fd7aba8cab61211ef0306
MazenAtlam <mazenatef5510@gmail.com>
2024-06-27 01:32:17 +0300

    Complete task 8

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from base_model import Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
@@ -10,3 +11,6 @@ class City(BaseModel, Base):
 
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+
+    places = relationship('Place', back_populates='cities',\
+                          cascade="all, delete-orphan")
