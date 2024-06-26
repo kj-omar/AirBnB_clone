12e39f8af8f262bb4367df4f7327fed2f0ec8569
MohamedAlaga <malaga.142014@gmail.com>
2024-06-27 02:03:32 +0300

    make the do_count and do show work with both file and db storage

--- console.py
+++ console.py
@@ -176,7 +176,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
         try:
-            print(storage.all[key])
+            print(storage.all()[key])
         except KeyError:
             print("** no instance found **")
 
@@ -230,9 +230,9 @@ class HBNBCommand(cmd.Cmd):
             for k, v in storage.all().items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
-        else:
-            for k, v in storage.all.items():
-                print_list.append(str(v))
+            else:
+                for k, v in storage.all().items():
+                    print_list.append(str(v))
 
         print(print_list)
 
@@ -244,7 +244,7 @@ class HBNBCommand(cmd.Cmd):
     def do_count(self, args):
         """Count current number of class instances"""
         count = 0
-        for k, v in storage.all.items():
+        for k, v in storage.all().items():
             if args == k.split('.')[0]:
                 count += 1
         print(count)
