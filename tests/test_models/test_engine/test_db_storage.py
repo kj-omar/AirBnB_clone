bebc78ddeed8d0624aa2580df58b4cb02f185136
MohamedAlaga <malaga.142014@gmail.com>
2024-06-27 02:24:39 +0300

    tests code style final

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -42,7 +42,7 @@ class TestDBStorageDocs(unittest.TestCase):
 test_db_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-        
+
 
 class TestDBStorageDocs(unittest.TestCase):
     """Tests to check the documentation and style of DBStorage class"""
