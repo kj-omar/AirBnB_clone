c31cefdc2f99bb7a3bc51cd6a27b3ace171f2aac
Terminal <terminalkarabo@gmail.com>
2024-06-26 12:22:55 -0700

    hbhb fixing

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,10 +1,15 @@
-import os
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+#!/usr/bin/python3
+from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
-from models.city import City
 from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 class DBStorage:
     __engine = None
--- models/state.py
+++ models/state.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 import models
-
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
-    name: input name
+        name: input name
     """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if models.storage_type == 'file':
-        @property
-        def cities(self):
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
