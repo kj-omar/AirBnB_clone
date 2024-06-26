c225f07e834fec153a9d8386069a5514ca633ebc
EuniceGeorge <georgeunice408.com>
2024-06-26 15:49:38 +0100

    task 3, 4

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- a script to create a database --
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create a user in localhost and set password --
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- set privileges for the user --
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- set SELECT privileges for user hbnb_dev on performance_schema --
+GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- a script to create a database --
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- create a user in localhost and set password --
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- set privileges for the user --
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- set SELECT privileges for user hbnb_test on performance_schema --
+GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
