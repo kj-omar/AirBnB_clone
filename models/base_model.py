3748b1cb974dcc9e83118579b247052dc4390f77
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-26 21:42:14 +0200

    Update [Add or replace in the class BaseModel with attributes]: BaseModel: (models/base_model.py)

--- models/base_model.py
+++ models/base_model.py
@@ -3,24 +3,47 @@
 import uuid
 from datetime import datetime
 
+# AirBnB_clone_v2 imports
+from sqlalchemy import Column, String, DateTime, MetaData
+from sqlalchemy.orm import declarative_base
 
-class BaseModel:
+# Create Base = declarative_base() before the class definition of BaseModel
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+class BaseModel():
     """A base class for all hbnb models"""
+    # create variable to store time format
+    TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
+    # Create class attributes for id, created at
+    id = Column(String(60), unique=True, primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            """
+            Initializes a new instance of the BaseModel class.
+
+            Args:
+                *args: Variable length argument list.
+                **kwargs: Arbitrary keyword arguments.
+
+            Returns:
+                None
+            """
+            if not kwargs:
+                from models import storage
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                storage.new(self)
+            else:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         self.TIME_FORMAT)
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         self.TIME_FORMAT)
+                del kwargs['__class__']
+                self.__dict__.update(kwargs)
 
     def __str__(self):
             """Returns a string representation of the instance.
