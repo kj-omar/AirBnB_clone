b9d22ca66d00161c2f5e6b9669cc0f7901133f5c
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-27 06:17:11 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""Review class module"""
-
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
+""" Review module for the HBNB project """
+import os
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel, Base):
-    """review class module for MySQL database
-
-    Inherits from SQLAlchemy Base and links to MySQL table reviews
-
-    Attributes:
-        __tablename__ (str): Name of MySQL table to store Reviews
-        text (sqlalchemy String): Review description
-        place_id (sqlalchemy String): Review's place id
-        user_id (sqlalchemy String): Review's user id
-    """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    place_id = Column(
+        String(60), ForeignKey('places.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    user_id = Column(
+        String(60), ForeignKey('users.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    text = Column(
+        String(1024), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
