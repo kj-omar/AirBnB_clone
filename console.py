9a52f901b9c955e039ec5399d76771b3245fdc82
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-06-27 16:42:20 -0500

    adding code for task 2

--- console.py
+++ console.py
@@ -118,11 +118,25 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
+        args_list = args.split()
+        class_name = args_list[0]
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn''t exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        kwargs = {}
+        for param in args_list[1:]:
+            key, value = param.split('=')
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1]
+                value = value.replace('\\\"', '"')
+                value = value.replace('_', ' ')
+            if '.' in value:
+                value = float(value)
+            elif value.isdigit():
+                value = int(value)
+            kwargs[key] = value
+
+        new_instance = HBNBCommand.classes[class_name](**kwargs)
         print(new_instance.id)
         storage.save()
 
