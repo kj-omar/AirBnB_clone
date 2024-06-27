e66a136ffc6368020bfd559501fc576a931d0692
fuadhasen <fuadhas6634@gmail.com>
2024-06-27 23:46:15 +0530

    [update] user model

--- models/user.py
+++ models/user.py
@@ -12,7 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship('Place', back_populates='user',
-                          cascade='all, delete')
-    reviews = relationship('Review', back_populates='user',
-                           cascade='all, delete')
