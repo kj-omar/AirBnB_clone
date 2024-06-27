9c7d8cd7f698b19d40c21006230dccff1f7b440c
azzaomer <azzaomer70@gmail.com>
2024-06-27 04:33:55 +0300

    6. DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -50,8 +50,8 @@ class BaseModel:
         dictionary.pop('_sa_instance_state')
         return dictionary
     def delete(self):
-    """
-    delete the current instance from the storage 
-    """
-    from models import storage
-    storage.delete(self)
+        """
+        delete the current instance from the storage 
+        """
+        from models import storage
+        storage.delete(self)
