b52d5aeaa6c84a85c2b6efb31a7c061efff4a031
azzaomer <azzaomer70@gmail.com>
2024-06-27 04:44:26 +0300

    mysql dev

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -26,7 +26,7 @@ class DBStorage:
         host = getenv('HBNB_MYSQL_HOST')
         db_name = getenv('HBNB_MYSQL_DB')
 
-        db_url = "mysql+mysqldb://{}:{}@{}/{}".format(username, password, db_name)
+        db_url = "mysql+mysqldb://{}:{}@{}/{}".format(username, password, host, db_name)
         self.__engine = create_engine(db_url, pool_pre_ping=True) #pool_per_ping checks if the connection is alive
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
