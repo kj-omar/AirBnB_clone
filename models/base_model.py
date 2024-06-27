d6e40715a9f3c1fd56b8307861e1bdd983ee6f17
azzaomer <azzaomer70@gmail.com>
2024-06-26 20:10:09 -0700

    6. DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -7,19 +7,21 @@ from sqlalchemy import Column, Integer, String, DateTime
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
 
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.now())
     updated_at = Column(DateTime, nullable=False, default=datetime.now())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()           
+            self.updated_at = datetime.now()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -49,9 +51,10 @@ class BaseModel:
         dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary.pop('_sa_instance_state')
         return dictionary
+
     def delete(self):
         """
-        delete the current instance from the storage 
+        delete the current instance from the storage
         """
         from models import storage
         storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -13,7 +13,8 @@ class State(BaseModel):
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
     else:
         @property
         def cities(self):
@@ -26,4 +27,4 @@ class State(BaseModel):
             for city in models.storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
-            return city_list    
+            return city_list
