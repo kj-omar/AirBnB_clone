86b25703f3a4124e795c53c45e4c3792f4fd65c7
azzaomer <azzaomer70@gmail.com>
2024-06-27 05:43:19 +0300

    6. DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from os import getenv
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
