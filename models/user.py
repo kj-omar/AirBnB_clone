030b428027f1c10d7b13a3798209e803799894b3
stringpappy <jejeodunayo70@gmail.com>
2024-06-28 05:40:12 -0700

    user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, String
 from .base_model import BaseModel
 from .base import Base
+from sqlalchemy.orm import relationship, back_populates
 
 class User(BaseModel, Base):
     __tablename__ = 'users'
@@ -9,3 +10,9 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    # Relationship to Review objects
+    reviews = relationship("Review",
+                           back_populates="user",
+                           cascade="all, delete-orphan",
+                           passive_deletes=True)
