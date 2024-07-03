dd29e9c61493e8108c3cf3aefaa973bbcd3a6c6c
LRWyatt801 <loganwyatt1995@gmail.com>
2024-07-03 14:17:05 -0500

    fix all function

--- console.py
+++ console.py
@@ -223,21 +223,18 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
-        print_list = []
-
-        if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+        args_list = shlex.split(args)
+        obj_list = []
+        if len(args_list) == 0:
+            obj_dict = storage.all()
+        elif args_list[0] in HBNBCommand.classes:
+            obj_dict = storage.all(HBNBCommand.classes[args_list[0]])
         else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
-
-        print(print_list)
+            print("** class doesn't exist **")
+            return False
+        for key in obj_dict:
+            obj_list.append(str(obj_dict[key]))
+        print(obj_list)
 
     def help_all(self):
         """ Help information for the all command """
