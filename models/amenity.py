a2dafade7c31f558a0d5a3d6c160428d7c5921aa
LRWyatt801 <loganwyatt1995@gmail.com>
2024-07-03 09:49:51 -0500

    Update amenity module for dbstorage

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,19 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
 
 class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    """Amenity class to store amenity information"""
+    # for database storage
+    if models.HBNB_TYPE_STORAGE == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                    viewonly=False)
+    # for json file storage
+    else:
+        name = ""
+        
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
