8a984e7ea0f458f7485538a28d5ada10f2b1c821
LRWyatt801 <loganwyatt1995@gmail.com>
2024-07-03 09:44:44 -0500

    Add documentation

--- models/review.py
+++ models/review.py
@@ -3,10 +3,21 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import models
 
 class Review(BaseModel, Base):
     """ Review class to store review information """
-    __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    # for database storage
+    if models.HBNB_TYPE_STORAGE == "db":
+        __tablename__ = 'reviews'
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    # for json file storage
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+        
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
