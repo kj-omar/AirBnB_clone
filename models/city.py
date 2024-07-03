aa9c52e5a013565c8934a3ac4a00636b8d2e4e87
LRWyatt801 <loganwyatt1995@gmail.com>
2024-07-03 09:53:41 -0500

    update class for dbstorage

--- models/city.py
+++ models/city.py
@@ -3,11 +3,20 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+import models
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete")
+    # for database storage
+    if models.HBNB_TYPE_STORAGE == "db":
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+    # for json file storage
+    else:
+        state_id = ""
+        name = ""
+        
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
