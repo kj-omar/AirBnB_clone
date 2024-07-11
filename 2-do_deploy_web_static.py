ca032e38500aadf87e53b3279a161bb37cfe4d54
Wongani <wonganichulu@gmail.com>
2024-07-11 18:33:36 +0200

    2-do_deploy_web_static.py: Updated deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,8 +3,7 @@
 
 from fabric.api import cd, env, put, run, sudo
 
-#env.hosts = ["ubuntu@35.153.93.227", "ubuntu@100.26.175.131"]
-env.hosts = ["ubuntu@35.153.93.227"]
+env.hosts = ["ubuntu@35.153.93.227", "ubuntu@100.26.175.131"]
 env.key_filename = "~/.ssh/id_rsa"
 
 
@@ -13,24 +12,28 @@ def do_deploy(archive_path):
 
     if archive_path is None:
         return(False)
-    #Upload file
-    put(archive_path, /tmp/)
-        pattern = r'([^/]+)\.tgz$'
+    # Upload file
+    put(archive_path, '/tmp/')
 
-    #Find filename and directory name from archive_path
+    # Find filename and directory name from archive_path
+    pattern = r'([^/]+)\.tgz$'
     directory = re.search(pattern, archive_path).group(1)
     filename = f"{directory}.tgz"
 
-    #Decompressing archive
-    run(f"mkdir -p /data/web_static/releases/{directory}")
-    run(f"tar -xzf /tmp/{filename}, -C /data/web_static/releases/{directory}/")
+    # Decompressing archive
+    run(f"mkdir -p /data/web_static/releases/{directory}/")
+    run(f"tar -xzf /tmp/{filename} -C /data/web_static/releases/{directory}/")
 
-    #Deleting archive
+    # Deleting archive
     run(f"rm /tmp/{filename}")
 
-
+    # Deploying new version
+    run(f"mv /data/web_static/releases/{directory}/web_static/* "
+        f"/data/web_static/releases/{directory}/")
+    run(f"rm -rf /data/web_static/releases/{directory}/web_static")
     run("rm -rf /data/web_static/current")
-    run(f"ln -s /data/web_static/releases/{directory} /data/web_static/current")
+    run(f"ln -s /data/web_static/releases/{directory}/ "
+        "/data/web_static/current")
 
 
 def do_pack():
@@ -39,4 +42,3 @@ def do_pack():
     name = f"web_static_{time}.tgz"
     local("mkdir -p ./versions")
     local(f"tar -czvf ./versions/{name} ./web_static")
-
