0a8251843da6227784d5a083eb3eff16d18046ec
Wongani <wonganichulu@gmail.com>
2024-07-11 14:56:28 +0200

    Added 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Fabric script that generates a .tgz archive from
+the contents of the web_static
+"""
+
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Creates a .tgz file"""
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = f"web_static_{time}.tgz"
+    local("mkdir -p ./versions")
+    local(f"tar -czvf ./versions/{name} ./web_static")
