114c97b22ca093d824b717609ce71974a4c1b1e8
fuadhasen <fuadhas6634@gmail.com>
2024-07-11 13:02:04 -0700

    [add] pack_web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive from the contents of the web_static
+"""
+
+from fabric.api import *
+import datetime
+
+def do_pack():
+    """ function to create  .tgz archive 
+    from the contents of the web_static.
+    """
+    local("mkdir -p versions")
+    time_stamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_{}.tgz".format(time_stamp))
+    res = local("tar -czf {} web_static".format(archive_name))
+    if res.succeeded:
+        return archive_name
+    else:
+        return None
+
+
