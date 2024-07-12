df27c10dee5d69ff50c0371af4757e5ee5e92356
fuadhasen <fuadhas6634@gmail.com>
2024-07-12 02:27:28 -0700

    [update] task2: deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,15 +11,18 @@ def do_deploy(archive_path):
     """ function to deploy archive """
     if archive_path:
         a_name = os.path.basename(archive_path)
-        a_folder_name = f_name.split('.')[0]
+        a_folder_name = a_name.split('.')[0]
 
         put(archive_path, "/tmp")
         # Uncompress the archive
-        sudo("tar -xzf /tmp/a_name -C /data/web_static/releases/{}".format(a_folder_name))
-        sudo(f'rm /tmp/{a_name}')
-        sudo('rm /data/web_static/current')
+        sudo("mkdir -p /data/web_static/releases/{}".format(a_folder_name))
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(a_name, a_folder_name))
+        sudo("rm /tmp/{}".format(a_name))
+        sudo("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(a_folder_name, a_folder_name))
+        sudo("rm -rf /data/web_static/releases/{}/web_static".format(a_folder_name))
+        sudo("rm /data/web_static/current")
 
-        sudo(f'ln -s /data/web_static/releases/{a_folder_name} /data/web_static/current')
+        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(a_folder_name))
         return True
     else:
         return False
