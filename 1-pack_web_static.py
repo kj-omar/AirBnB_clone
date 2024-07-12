c4ad68a4f1e346c60044774cb3fd188670c7cea1
Wongani <wonganichulu@gmail.com>
2024-07-12 11:24:14 +0200

    1-pack_web_static.py: Deleted commented code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,12 +9,8 @@ from fabric.api import local
 
 
 def do_pack():
-    """Creates a .tgz file
-    time = datetime.now().strftime("%Y%m%d%H%M%S")
-    name = f"web_static_{time}.tgz"
-    local("mkdir -p ./versions")
-    local(f"tar -czvf ./versions/{name} ./web_static")
-    """
+    """Creates a .tgz file"""
+
     time = datetime.now().strftime("%Y%m%d%H%M%S")
     name = f"web_static_{time}.tgz"
     zip_path = f"versions/{name}"
