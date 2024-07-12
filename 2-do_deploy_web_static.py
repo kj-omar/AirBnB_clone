e9a21fc3e3d5ab4b99708cb93a6c68180cec4e82
fuadhasen <fuadhas6634@gmail.com>
2024-07-12 10:03:36 -0700

    [update] task2: do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive from the contents of the web_static
+""" Fabric script that generates a .tgz archive
+from the contents of the web_static
 """
 import os
 from fabric.api import env, run, put, sudo
@@ -13,17 +14,21 @@ def do_deploy(archive_path):
     if archive_path:
         a_name = os.path.basename(archive_path)
         a_folder_name = a_name.split('.')[0]
+        path = "/data/web_static/releases/"
 
         put(archive_path, "/tmp")
         # Uncompress the archive
         sudo("mkdir -p /data/web_static/releases/{}".format(a_folder_name))
-        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(a_name, a_folder_name))
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+              a_name, a_folder_name))
         sudo("rm /tmp/{}".format(a_name))
-        sudo("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(a_folder_name, a_folder_name))
-        sudo("rm -rf /data/web_static/releases/{}/web_static".format(a_folder_name))
+        sudo("mv {0}{1}/web_static/* {0}{1}".format(path, a_folder_name))
+        sudo("rm -rf /data/web_static/releases/{}/web_static".format(
+              a_folder_name))
         sudo("rm /data/web_static/current")
 
-        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(a_folder_name))
+        sudo("ln -s {0}{1}/ /data/web_static/current".format(
+              path, a_folder_name))
         return True
     else:
         return False
