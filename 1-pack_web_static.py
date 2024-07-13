d07d3168618633b4912771d8cb6858a63059bda9
stringpappy <jejeodunayo70@gmail.com>
2024-07-13 07:28:53 -0700

    airbnb_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from fabric.api import local
 import os.path
 
+
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     date = datetime.utcnow()
