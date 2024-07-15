705924d84c2d96c9ae919362ebeb2a7e95790631
fuadhasen <fuadhas6634@gmail.com>
2024-07-15 09:50:14 -0700

    [update] task3: full_deployement

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,8 @@ import os
 from fabric.api import env, run, put, sudo
 import datetime
 
-env.hosts = ['100.26.152.53', '35.174.208.133']
+if __name__ == "__main__":
+    env.hosts = ['100.26.152.53', '35.174.208.133']
 
 
 def do_deploy(archive_path):
@@ -18,16 +19,16 @@ def do_deploy(archive_path):
 
         put(archive_path, "/tmp")
         # Uncompress the archive
-        sudo("mkdir -p /data/web_static/releases/{}".format(a_folder_name))
-        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+        run("mkdir -p /data/web_static/releases/{}".format(a_folder_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
               a_name, a_folder_name))
-        sudo("rm /tmp/{}".format(a_name))
-        sudo("mv {0}{1}/web_static/* {0}{1}".format(path, a_folder_name))
-        sudo("rm -rf /data/web_static/releases/{}/web_static".format(
+        run("rm /tmp/{}".format(a_name))
+        run("mv {0}{1}/web_static/* {0}{1}".format(path, a_folder_name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(
               a_folder_name))
-        sudo("rm /data/web_static/current")
+        run("rm /data/web_static/current")
 
-        sudo("ln -s {0}{1}/ /data/web_static/current".format(
+        run("ln -s {0}{1}/ /data/web_static/current".format(
               path, a_folder_name))
         return True
     else:
