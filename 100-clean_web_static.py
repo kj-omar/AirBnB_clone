d163f2998c9aef6285e9567f57cd1392d263fd94
hakeem-hassan <hlanre@ymail.com>
2024-07-14 19:44:02 -0700

    update

---
+++ 100-clean_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Deletes out of date archives
+"""
+
+import os
+from fabric.api import *
+
+env.hosts = ["3.235.239.225", "35.170.64.18"]
+env.user = 'ubuntu'
+env.identinty = '~/.ssh/school'
+
+
+def do_clean(number=0):
+    """
+    Delete out-of-date archives.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
