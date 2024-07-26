71f53192cf25cd19309911931bc7c86668a9d790
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-26 18:42:19 +0100

    python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,21 +1,27 @@
+#!/usr/bin/python3
 """ Start Flask application """
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Display Hello HBNB! """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Display HBNB """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ Display C followed by the value of the text variable """
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
