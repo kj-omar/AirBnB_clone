7a5c7526dbbb190de658e7e57a4e4904e05f2d08
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-07-26 14:08:30 +0200

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,13 +5,6 @@ A simple Flask web application script.
 This script initializes a Flask web application with a single route
 that responds with "Hello HBNB!" when accessed.
 
-Configuration:
-- `app.url_map.strict_slashes` is set to `False` to allow URLs with
-  or without a trailing slash to match the same route.
-
-The application runs with the following settings:
-- `debug=True` for development auto-reloading and better error messages.
-- The server listens on all network interfaces (`0.0.0.0`) on port `5000`.
 """
 
 from flask import Flask
@@ -35,8 +28,5 @@ def hello_hbnb():
 
 
 if __name__ == '__main__':
-    # Run the Flask application
-    # The `debug=True` argument enables debug mode.
-    # `host='0.0.0.0'` allows access from any network interface.
-    # `port=5000` specifies the port on which the server will listen.
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
