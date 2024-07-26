5d67bffc808b14b2f194c7d5e004be3e3407a26c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-26 18:40:17 +0100

    c/<text> route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,16 +1,20 @@
+#!/usr/bin/python3
 """ Start Flask application """
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Display Hello HBNB! """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Display HBNB """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
