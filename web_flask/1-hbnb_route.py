17720ac5f2e1523a7b6e1c147d1f1e2a372a7041
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-26 18:38:46 +0100

    hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,20 @@
+#!/usr/bin/python3
 """ Start Flask application """
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Display Hello HBNB! """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Display HBNB """
     return 'HBNB'
-
 
 
 if __name__ == '__main__':
