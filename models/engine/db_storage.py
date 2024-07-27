e5f9814ec8f1d46cc1132905678fe35098b24dca
Wongani <wonganichulu@gmail.com>
2024-07-27 22:20:42 +0200

    Added close methods to file_storage.py and db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -81,3 +81,7 @@ class DBStorage:
         factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(factory)
         self.__session = Session()
+
+    def close(self):
+        """ closes current session if active """
+        self.__session.remove()
