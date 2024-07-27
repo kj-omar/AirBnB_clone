604b505d8a83c7c48a3e6cd84b236c79f7dea49c
Wongani <wonganichulu@gmail.com>
2024-07-27 18:55:51 +0200

    Added 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" A flask Application that listens on 0.0.0.0, port 5000 """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Hello Route"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a string"""
+    return("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_Text(text):
+    """Returns a string"""
+    return("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_text(text="is cool"):
+    """Returns a string"""
+    return("Python {}".format(text.replace("_", " ")))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_num(n):
+    """Checks if n is a number"""
+    if isinstance(n, int):
+        return ("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
