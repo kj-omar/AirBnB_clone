67f34c40341171301ddcc76fd87d9754aa4ae5c1
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-07-27 17:56:55 +0200

    Update File: models/engine/file_storage.py, models/engine/db_storage.py, models/state.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,17 +69,10 @@ class FileStorage:
     def delete(self, obj=None):
         """Delete obj from __objects if it’s inside"""
         if obj is not None:
-            try:
-                del self.__objects["{}.{}".format(
-                    type(obj).__name__, obj.id)]
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
                 self.save()
-            except (AttributeError, KeyError):
-                pass
-        # if obj is not None:
-        #     key = obj.__class__.__name__ + '.' + obj.id
-        #     if key in self.__objects:
-        #         del self.__objects[key]
-        #         self.save()
     
     def close(self):
         """ Calls the Realod Method """
