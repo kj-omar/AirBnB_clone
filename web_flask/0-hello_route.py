da383e138997c802a04aba46a39a2fe3982c2cb8
Wongani <wonganichulu@gmail.com>
2024-07-27 18:16:55 +0200

    0-hello_route.py: Added

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A flask hello world app """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Hello view"""
+    return ("Hello HBNB!")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
