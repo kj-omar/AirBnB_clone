90303a5538733ac7b4b781a89c87bed810b9bd4e
Wongani <wonganichulu@gmail.com>
2024-07-28 10:22:51 +0200

    Added web_flask/7-states_list.py and web_flask/templates/7-states_list.html

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Starts a flask app that uses db storage"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("states_list", strict_slashes=False)
+def list_states():
+    """Renders 7-states_list.html"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Ends current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
