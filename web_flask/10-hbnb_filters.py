640d7af3d324002068f557edbfa5a5b5f55f0259
Wongani <wonganichulu@gmail.com>
2024-07-28 17:43:35 +0200

    Added fixes

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, render_template
 from models import storage
+from models import *
 from models.state import State
 from models.amenity import Amenity
 
@@ -13,14 +14,14 @@ app = Flask(__name__)
 @app.route("/hbnb_filters", strict_slashes=False)
 def filters():
     """Renders 10-hbhb_filters.html"""
-    states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
     return render_template('10-hbnb_filters.html',
                            states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exception):
     """Ends current SQLAlchemy Session"""
     storage.close()
 
