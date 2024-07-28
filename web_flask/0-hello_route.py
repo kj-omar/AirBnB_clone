18d3a53dd563186b47e68795be520272d36356cd
MohamedAlaga <malaga.142014@gmail.com>
2024-07-29 01:41:30 +0300

    hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
 "this is a python script module"
@@ -7,3 +8,6 @@ app = Flask(__name__)
 def hello():
     "first flask funciton"
     return "Hello HBNB!$"
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
