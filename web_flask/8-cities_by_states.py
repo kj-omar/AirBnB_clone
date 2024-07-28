4eefa509ef57f079cb2a86063cb157a2da9bb2d9
fuadhasen <fuadhas6634@gmail.com>
2024-07-29 00:40:26 +0530

    add task 8: cities_by_states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" python script that starts a Flask
+web application
+"""
+from models import storage
+from models.state import State
+from flask import Flask, abort, render_template
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def State_city():
+    """ state route """
+    new_dict = storage.all("State")
+    states = list(new_dict.values())
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """close the database connection """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
