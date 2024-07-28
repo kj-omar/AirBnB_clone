a085d8e28e23672682bc9c7fc85d6e0921f0ff5f
stringpappy <jejeodunayo70@gmail.com>
2024-07-28 13:44:29 -0700

    web_flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,8 +5,10 @@ Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-/python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+/c/<text>: display “C ”, followed by the value of the text
+variable (replace underscore _ symbols with a space )
+/python/(<text>): display “Python ”, followed by the value of the
+text variable (replace underscore _ symbols with a space )
 The default value of text is “is cool”
 /number/<n>: display “n is a number” only if n is an integer
 /number_template/<n>: display a HTML page only if n is an integer:
@@ -58,6 +60,7 @@ def number(n):
 def number_template(n):
     """To Displays an HTML page only if <n> is an integer."""
     return render_template("5-number.html", n=n)
+
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
