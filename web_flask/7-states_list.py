9836d4c72511c29fe536128c0617a87848790610
stringpappy <jejeodunayo70@gmail.com>
2024-07-31 15:06:06 -0700

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""The  application module that listens on 0.0.0.0, port 5000.
+
+""" The  application module that listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
