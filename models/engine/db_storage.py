936e1c72bd98474eb0e7413165d802697ed872a3
fuadhasen <fuadhas6634@gmail.com>
2024-08-01 00:07:10 +0530

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,9 +77,9 @@ class DBStorage:
         """ create tables in the Database """
         from models.base_model import Base
         Base.metadata.create_all(self.__engine)
-        self.__session = sessionmaker(bind=self.__engine,
-                                      expire_on_commit=False)
-        self.__session = scoped_session(self.__session)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
 
     def close(self):
         """close method ro manage resources"""
