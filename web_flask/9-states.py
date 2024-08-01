2d8a15eab29ab2f3fba89b2e24932d0bb8698fa1
stringpappy <jejeodunayo70@gmail.com>
2024-07-31 22:00:43 -0700

    webflask

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,20 +4,32 @@ Starts a Flask web application.
 The application should listens on 0.0.0.0, port 5000.
 """
 from flask import Flask
+from models import storage
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
+@app.route("/states", strict_slashes=False)
+def states()
     """ To Displays 'Hello HBNB!'."""
-    return "Hello HBNB!"
-
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
+def states_id(id):
     """ To Displays 'HBNB'."""
-    return "HBNB"
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+
+    storage.close()
 
 
 if __name__ == "__main__":
