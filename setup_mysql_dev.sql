5ec61953f215dec0fc72da7e75028c4f3ce89094
Icelycoool <mohammedcali@proton.me>
2024-08-04 23:14:17 +0300

    3. MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,10 +9,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges on the hbnb_dev_db database to the new user
-GRANT ALL PRIVELEGES ON  `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON  `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database to the new user
-GRANT SELECT ON `performance_schema`.* to 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to ensure that all changes take effect
 FLUSH PRIVILEGES;
