f629c97931f8ff62b743aa067408bf9f31983886
Icelycoool <mohammedcali@proton.me>
2024-08-04 23:11:45 +0300

    3. MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,18 @@
+-- Prepares a MySQL server for the AirBnB_clone_v2
+-- Database: hbnb_dev_db
+-- New user: hbnb_dev
+
+-- Create the database if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it does not already exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the hbnb_dev_db database to the new user
+GRANT ALL PRIVELEGES ON  `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the new user
+GRANT SELECT ON `performance_schema`.* to 'hbnb_dev'@'localhost';
+
+-- Flush privileges to ensure that all changes take effect
+FLUSH PRIVILEGES;
