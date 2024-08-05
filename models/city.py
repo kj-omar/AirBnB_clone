791404b373bb1457ef1e5b14ec74b269d6423990
Icelycoool <mohammedcali@proton.me>
2024-08-05 22:11:16 +0300

    Updated the base model and the city mdel

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import os
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    if os.getenv('HBNB_TY_STORAGE') == 'db':
+        __tablename__ == 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+        places = relationship('Place', backref="cities", CASCADE='all')
+    else:
+        state_id = ""
+        name = ""
