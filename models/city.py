538b034e944b29cffb8dd14d4f8493c71ddf72b8
Icelycoool <mohammedcali@proton.me>
2024-08-05 22:51:50 +0300

    Updated the city models

--- models/city.py
+++ models/city.py
@@ -4,16 +4,17 @@ import os
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+
     if os.getenv('HBNB_TY_STORAGE') == 'db':
         __tablename__ == 'cities'
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
-        places = relationship('Place', backref="cities", CASCADE='all')
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref="cities", cascade='all, delete-orphan')
     else:
         state_id = ""
         name = ""
