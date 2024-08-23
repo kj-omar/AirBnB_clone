5325e903fe6cfe6f63d59c25658bd788f6be49e5
gichobih <dennismuri254@gmail.com>
2024-08-23 02:11:00 -0700

    0x1A-application_server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,6 @@ start Flask application
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
-
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def airbnb_onepage():
     """returns Hello HBNB for /airbnb-onepage/"""
